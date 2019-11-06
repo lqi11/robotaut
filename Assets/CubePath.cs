3c9eb888b580d670b45f908c7262e56bafa4cfd5
Lin Qi <qi-lin@outlook.com>
2019-11-06 15:45:01 -0600

    first commit

---
+++ Assets/CubePath.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+
+public class CubePath : MonoBehaviour
+{
+
+    bool toolbox = false;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            this.transform.Translate(Vector3.forward * 0.04f);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.down * Time.deltaTime * 20);
+            //this.transform.RotateAround(Vector3.zero, Vector3.up, 20 * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.up * Time.deltaTime * 20);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            this.transform.Translate(Vector3.back * 0.04f);
+        }
+
+
+        // fetch toolbox
+        speed = 0.001f;
+        float step = speed * Time.deltaTime;
+        float toolboxX = 1.089638f;
+        float toolboxY = -0.38f;
+        float toolboxZ = 23.8f;
+        Vector3 startPosition = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+        Vector3 destPosition = new Vector3(toolboxX,toolboxY,toolboxZ);
+        if (Input.GetKey(KeyCode.T))
+        {
+            toolbox = true;
+            if (toolbox)
+        {
+            transform.position = Vector3.MoveTowards(startPosition, destPosition, step);
+        }
+        }
+        
+        
+    }
+    
+    void OnCollisionEnter(Collision otherObj)
+    {
+
+    }
+    
+}
---
+++ CubePath_1105version.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+
+public class CubePath : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*       if (Input.GetKey(KeyCode.W))
+               {
+                   this.transform.Translate(Vector3.forward * 0.04f);
+               }
+
+               if (Input.GetKey(KeyCode.A))
+               {
+                   transform.Rotate(Vector3.down * Time.deltaTime * 20);
+                   //this.transform.RotateAround(Vector3.zero, Vector3.up, 20 * Time.deltaTime);
+               }
+
+               if (Input.GetKey(KeyCode.D))
+               {
+                   transform.Rotate(Vector3.up * Time.deltaTime * 20);
+               }
+
+               if (Input.GetKey(KeyCode.S))
+               {
+                   this.transform.Translate(Vector3.back * 0.04f);
+               }
+       */
+        transform.position += transform.forward * 5 * Time.deltaTime;
+        transform.Rotate(new Vector3(0f, -100f, 0f) * Time.deltaTime);
+    }
+    
+    void OnCollisionEnter(Collision otherObj)
+    {
+
+    }
+}
+//To Do List.
+//Adding script to Solar Panels (Nick)
+//Adding script to Robot and storage area (Lin)
+//Making the robot move automatically.  Programming a path. (All)
+//Adding Tools and toolbox models (Nick)
+//Make robot move around crater (Andrew)
+//Storage area domed (Lin)
+// Find coordinates of objects.
+// Due by Nov 13.
+//Crater: (0, -50, 0) Radius of 26.25
+//Storage: (50, 7.822609, -50)
+// Cube (0, 1, -35)
+//Get or make models for crates.
+//Get or make models of something with wheels.
